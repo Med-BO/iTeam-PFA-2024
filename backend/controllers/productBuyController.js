9dafcbece48c454e5a9dec5bc9a89819bb08ed00
Bjaoui-mohamed-aziz <azizbjaoui312@gmail.com>
2024-05-28 09:30:34 +0200

    fixed and tested the BuyProduct api

--- backend/controllers/productBuyController.js
+++ backend/controllers/productBuyController.js
@@ -3,12 +3,15 @@ import asyncHandler from "express-async-handler";
 import mongoose from "mongoose";
 import ProductBuy from "../models/productBuyModel.js";
 import ProductInsurance from "../models/productInsuranceModel.js";
+import moment from "moment";
 
 const app = express();
 
 const BuyProduct = asyncHandler(async (req, res) => {
-  const { contractType, ppd, stealProtection, beginDate, endDate } = req.body;
+  let {Product,User, contractType, ppd, stealProtection, beginDate, endDate } = req.body;
 
+  beginDate = moment(beginDate, 'DD/MM/YYYY').toDate();
+  endDate = moment(endDate, 'DD/MM/YYYY').toDate();
   let productInsurance;
   if (contractType) {
     // create insurance
@@ -25,16 +28,11 @@ const BuyProduct = asyncHandler(async (req, res) => {
   const productBuy = await ProductBuy.create({
     productInsurance: contractType ? productInsurance._id : null,
     Product,
+    User,
     date: Date.now(),
   });
   if (productBuy) {
-    res.status(201).json({
-      contractType: product.contractType,
-      ppd: product.ppd,
-      stealProtection: product.stealProtection,
-      beginDate: product.beginDate,
-      endDate: product.endDate,
-    });
+    res.status(201).json(productBuy);
   } else {
     res.status(400);
     throw new Error("Invalid product data");
